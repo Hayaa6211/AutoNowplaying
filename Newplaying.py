37de6ca3a121eab979fff8c25285df5921720de3
Hayaa6211 <astronautryosuzuki@gmail.com>
2021-04-08 15:37:39 +0900

    ほのけ好き

--- Newplaying.py
+++ Newplaying.py
@@ -1,15 +1,19 @@
+import os
 import ast
 import json
 import tweepy
 from datetime import datetime
 from unicodedata import east_asian_width
-
+from matplotlib import pyplot as plt
+from dotenv import load_dotenv
 from PIL import Image
 
-consumer_key='Kj23CTidCKjrWIohX1mEvn37E'
-consumer_secret='aBCBtdv62HGGls4l6O3503IXkbxO34LXukbcsM1Nlh8kiwTCq9'
-access_token='877115268631113729-DrU9fQIyM8q0kEFAFhcuXZ7PNDVTogk'
-access_token_secret='cNUWCokDGdZ4O7w4xGcd8bRBPh6e1pdKJF59tE4CUVCby'
+load_dotenv(".env")
+
+consumer_key= os.environ.get("consumer_key")
+consumer_secret=os.environ.get("consumer_secret")
+access_token=os.environ.get("access_token")
+access_token_secret=os.environ.get("access_token_secret")
 
 
 dt = datetime.now()
@@ -17,22 +21,23 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-Artist_file = "C:/Users/admin/iCloudDrive/iCloud~is~workflow~my~workflows/artist.txt"
-title_file = "C:/Users/admin/iCloudDrive/iCloud~is~workflow~my~workflows/title.txt"
-pic_file = "C:/Users/admin/iCloudDrive/iCloud~is~workflow~my~workflows/jacket.png"
-db_file = "C:/Users/admin/Documents/A_Python/Auto_Nowplaying/archive.json"
+Artist_file = "C:/Users/hayaa/iCloudDrive/iCloud~is~workflow~my~workflows/artist.txt"
+title_file = "C:/Users/hayaa/iCloudDrive/iCloud~is~workflow~my~workflows/title.txt"
+pic_file = "C:/Users/hayaa/iCloudDrive/iCloud~is~workflow~my~workflows/jacket.png"
+db_file = "C:/Users/hayaa/Documents/A_Python/Auto_Nowplaying/archive.json"
+graph_file = "graph.png"
 music_db = dict()
 
 tweet = f"#僕の聴いてる音楽\n\n{dt.month}/{dt.day}\n"
 
-def make_image(pic_file):   #完成
+def make_image(pic_file):   #画像を持ってくる
     img = Image.open(pic_file)
     img_resize_lanczos = img.resize((img.width//2, img.height//2), Image.LANCZOS)
-    img_resize_lanczos.save('C:/Users/admin/iCloudDrive/iCloud~is~workflow~my~workflows/jacket_resize.png')
-    pic_file_resize = 'C:/Users/admin/iCloudDrive/iCloud~is~workflow~my~workflows/jacket_resize.png'
+    img_resize_lanczos.save('C:/Users/hayaa/iCloudDrive/iCloud~is~workflow~my~workflows/jacket_resize.png')
+    pic_file_resize = 'C:/Users/hayaa/iCloudDrive/iCloud~is~workflow~my~workflows/jacket_resize.png'
     return pic_file_resize
 
-def Artist_read(Artist_file):   #完成
+def Artist_read(Artist_file):   #アーティストのリスト
     with open(Artist_file,"r",encoding="utf-8") as f:
         Artist_list = f.read()
         Artist_list = Artist_list.split("|")
@@ -42,13 +47,13 @@ def Artist_read(Artist_file):   #完成
                 Artist_list[n] = i[:13] + "..."
     return Artist_list
 
-def title_read(title_file):     #完成
+def title_read(title_file):     #タイトルのリスト
     with open(title_file,"r",encoding="utf-8") as f:
         title_list = f.read()
         title_list = title_list.split("|")
     return title_list
 
-def counter(tweet):     #完成
+def counter(tweet):     #文字のカウンター
     tweet_list = list(tweet)
     text_counter = 0
     for n in tweet_list:
@@ -67,7 +72,7 @@ def counter(tweet):     #完成
             text_counter += 1
     return text_counter
 
-def make_tweet(Artist_list,title_list):     #完成
+def make_tweet(Artist_list,title_list):    #ツイート文の作成
     global tweet
     for Artist,title in zip(Artist_list,title_list):
         text_counter = counter(tweet)
@@ -76,26 +81,35 @@ def make_tweet(Artist_list,title_list):     #完成
         else:
             break
 
+def make_graph(music_db):
+    x = []
+    y = []
+    for i in music_db:
+        x.append(i)
+        y.append(music_db[i])
+    plt.bar(x,y)
+    plt.xticks(rotation=270)
+    plt.savefig(graph_file)
 
-def do_tweet(file,tweet):
+def do_tweet(file,tweet):   #実際にツイートする
     try:
         api.update_with_media(filename = file, status = tweet)
     except:
         api.update_status(status = "自動ツイートが文字数制限でできなかったので\nほのけのTwitterを見ててください\nhttps://twitter.com/_kuroki_honoka?s=20")
     
-def add_db(Artist_list,music_db):   #動いてる
-    for i,n in enumerate(Artist_list):
+def add_db(Artist_list,music_db):   #dbに追加する(ここはjson)
+    for n in range(len(Artist_list)):
         if not n in music_db:
             music_db[n] = 1
         else:
             music_db[n] += 1
 
-def open_db():  #動いてる?
+def open_db():  #jsonからdictにする
     with open(db_file,encoding="utf-8") as f:
         music_db = json.load(f)
     return music_db
 
-def write_db(music_db):     #動いてる?
+def write_db(music_db):     #書き込む
     with open(db_file,"w",encoding="utf-8") as f:
         if dt.day != 1:
             music_json = json.dumps(music_db,ensure_ascii=False,indent=4)
@@ -103,14 +117,14 @@ def write_db(music_db):     #動いてる?
         else:
             f.write("{}")
 
-def count_music(music_db):    #動いてる
+def count_music(music_db):    #何回聴いたか数える
     count = sum(music_db.values())
     return count
 
-def tweet_first(music_db):
-    if dt.day == 17:
+def tweet_first(music_db):    #月初めのツイート
+    if dt.day == 1:
         text = f"#僕の聴いた音楽\n\n{dt.month - 1}月聞いたアーティスト\n\n"
-        music_db = sorted(music_db.item(),key=lambda x:x[0])
+        
         for i in music_db:
             text_counter = counter(text)
             if text_counter < 200:
@@ -118,9 +132,9 @@ def tweet_first(music_db):
             else:
                 break
             count = count_music(music_db)
-
         text += f"\nその他{count}曲聞きました"
-        api.update_status(status = text)
+        make_graph(music_db)
+        api.update_with_media(filename = graph_file, status = text)
 
 
 def main():
@@ -129,7 +143,7 @@ def main():
     Artist_list = Artist_read(Artist_file) #アーティストのリスト作成
     title_list = title_read(title_file) #タイトルのリスト作成
     make_tweet(Artist_list,title_list) #ツイート文作成
-    
+    do_tweet(pic_file_resize,tweet)
     music_db = open_db() #データベース開くよ
     tweet_first(music_db) #月初めのツイート
     add_db(Artist_list,music_db) #データベースに追加するよ
